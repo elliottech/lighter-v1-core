57080d8df4fce69118e861440cbb29487f6dedbc
kanth <defi.kanth@gmail.com>
2023-04-11 14:42:39 +0530

    feat: add balance assertions for BidMarketOrder Test

--- test/MarketOrder.ts
+++ test/MarketOrder.ts
@@ -59,8 +59,10 @@ describe("OrderBook contract, market orders", function () {
 
     // Create the order book
     const sizeTick = 100; // decimal=3 so multiples of 0.1
-    const priceTick = 10; // decimal=3 so multiples of 0.001
+    const priceTick: number = 10; // decimal=3 so multiples of 0.001
     await factory.createOrderBook(token0.address, token1.address, 2, 1);
+
+    const priceMultiplier = (priceTick * sizeTick) / (10 ** 3);
 
     return {
       router,
@@ -71,26 +73,80 @@ describe("OrderBook contract, market orders", function () {
       acc2,
       sizeTick,
       priceTick,
+      priceMultiplier,
     };
   }
 
-  it("Market orders tests by filling ask limit orders", async function () {
-    const { router, acc1 } = await get_setup_values();
+  it("Bid-Market-Order match with Ask-Limit-Orders", async function () {
+    const { router, token0, token1, acc1, acc2, sizeTick, priceMultiplier } = await get_setup_values();
+    const marketMaker = acc2;
+    const marketTaker = acc1;
+    const td = BigInt(1000000); // token decimals
 
-    // Create 5 ask limit orders
-    await router.connect(acc1).createLimitOrder(0, 1, 1, 1, 0); // 2
-    await router.connect(acc1).createLimitOrder(0, 1, 1, 1, 0); // 3
-    await router.connect(acc1).createLimitOrder(0, 1, 1, 1, 0); // 4
-    await router.connect(acc1).createLimitOrder(0, 1, 1, 1, 0); // 5
-    await router.connect(acc1).createLimitOrder(0, 1, 1, 1, 0); // 6
+    const token1_balance_bef_limit_order_market_maker = await token1.balanceOf(marketMaker.address);
+    const token0_balance_bef_limit_order_market_maker = await token0.balanceOf(marketMaker.address);
 
-    // Create market order to fill first three asks
-    await router.connect(acc1).createMarketOrder(0, 3, 1, 0);
+    const orderBookId: number = 0;
+    const amount0base_limit: number = 1;
+    const price0Base_limit: number = 1;
+    const isAsk_limit: number = 1;
+    const hintId: number = 0;
+
+    // Create 5 ask-limit orders
+    await router.connect(marketMaker).createLimitOrder(orderBookId, amount0base_limit, price0Base_limit, isAsk_limit, hintId); // 2
+    await router.connect(marketMaker).createLimitOrder(orderBookId, amount0base_limit, price0Base_limit, isAsk_limit, hintId); // 3
+    await router.connect(marketMaker).createLimitOrder(orderBookId, amount0base_limit, price0Base_limit, isAsk_limit, hintId); // 4
+    await router.connect(marketMaker).createLimitOrder(orderBookId, amount0base_limit, price0Base_limit, isAsk_limit, hintId); // 5
+    await router.connect(marketMaker).createLimitOrder(orderBookId, amount0base_limit, price0Base_limit, isAsk_limit, hintId); // 6
+
+    const token1_balance_aft_limit_order_market_maker = await token1.balanceOf(marketMaker.address);
+    const token0_balance_aft_limit_order_market_maker = await token0.balanceOf(marketMaker.address);
+    
+    //assert the balance of token0 for market-maker
+    //after submitting 5 limit orders (ask - token0) -> with token-amount:1 for each limit-order, 
+    //the balance of token0 for market-maker should decrease by 5 * sizeTick
+    expect(
+      (token0_balance_aft_limit_order_market_maker).eq(
+        BigInt(token0_balance_bef_limit_order_market_maker) - BigInt(5 * sizeTick)
+      )
+    ).to.be.true;
+
+    const token1_balance_bef_mkt_order_market_taker = await token1.balanceOf(marketTaker.address);
+    const token0_balance_bef_mkt_order_market_taker = await token0.balanceOf(marketTaker.address);
+  
+    const amount0base_bid_market: number = 3;
+    const price0Base_bid_market: number = 1;
+    const isAsk_bid_market: number = 0;
+
+    // Create bid-market order to fill first 3 limit-ask orders
+    // market-order to buy token0 of amount: 3 after including size-tick it will be: 3 * 100 = 300
+    await router.connect(marketTaker).createMarketOrder(orderBookId, amount0base_bid_market, price0Base_bid_market, isAsk_bid_market);
 
     let order_ids = await router.getLimitOrders(0);
 
+    const token0_balance_aft_mkt_order_market_taker = await token0.balanceOf(marketTaker.address);
+    const token1_balance_aft_mkt_order_market_maker = await token1.balanceOf(marketMaker.address);
+    
     expect(order_ids[0].length).to.equal(2);
     expect(order_ids[0]).to.deep.equal([5, 6]);
+    //assert the balance of token0 for market-taker
+    //after matching with 3 limit orders, 
+    //the balance of token0 for market-taker should increase by 300 amount
+    expect(
+      (token0_balance_aft_mkt_order_market_taker).eq(
+        BigInt(token0_balance_bef_mkt_order_market_taker) + BigInt(3 * sizeTick)
+      )
+    ).to.be.true;
+
+    //assert the balance of token1 for market-maker
+    //after matching bid-market-order with 3 ask-limit-orders, 
+    //the balance of token1 for market-maker should increase by 3 amount
+    expect(
+      (token1_balance_aft_mkt_order_market_maker).eq(
+        BigInt(token1_balance_aft_limit_order_market_maker) + BigInt(3 * amount0base_limit * price0Base_limit * priceMultiplier)
+      )
+    ).to.be.true;
+
   });
 
   it("Market orders tests by filling bid limit orders", async function () {
@@ -112,7 +168,7 @@ describe("OrderBook contract, market orders", function () {
     expect(order_ids[0]).to.deep.equal([5, 6]);
   });
 
-  it("Market order tests with fallback function", async function () {
+  it("Market-Bid order tests with fallback function", async function () {
     const { router, token0, token1, acc1, acc2 } = await get_setup_values();
 
     const tokenAmount = "10000000000000";
@@ -120,6 +176,7 @@ describe("OrderBook contract, market orders", function () {
     const td = BigInt(1000000); // token decimals
     const startBalance = BigInt(tokenAmount);
 
+    //send market-ask order with hintId: 0, amount(token0): 15 tokens at limit-price 1
     await (
       await acc1.sendTransaction({
         to: router.address,
@@ -134,6 +191,7 @@ describe("OrderBook contract, market orders", function () {
       })
     ).wait();
 
+    //send market-ask order with hintId: 1, amount(token0): 25 tokens at limit-price 1
     await (
       await acc1.sendTransaction({
         to: router.address,
@@ -148,6 +206,7 @@ describe("OrderBook contract, market orders", function () {
       })
     ).wait();
 
+    //send market-ask order with hintId: 3, amount(token0): 5 tokens at limit-price 1
     await (
       await acc1.sendTransaction({
         to: router.address,
@@ -162,6 +221,7 @@ describe("OrderBook contract, market orders", function () {
       })
     ).wait();
 
+    //send market-Bid order for 45 token0 tokens (current market price: 1)
     await (
       await acc2.sendTransaction({
         to: router.address,
@@ -174,11 +234,16 @@ describe("OrderBook contract, market orders", function () {
       })
     ).wait();
 
+    //assert the balance of token0 for market-taker
+    //after matching with 3 limit orders, the balance of token0 should increase by 45*100 amount
     expect(
       (await token0.balanceOf(acc2.address)).eq(
         startBalance + BigInt(4500) * td
       )
     ).to.be.true;
+
+    //assert the balance of token1 for market-taker
+    //after matching with 3 limit orders, the balance of token1 should decrease by 15*1+25*1+5*1
     expect(
       (await token1.balanceOf(acc2.address)).eq(startBalance - BigInt(135) * td)
     ).to.be.true;
